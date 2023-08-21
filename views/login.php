3379bb28cea164015950bd0dd0a3b9b6e706e553
Maria Jose <mariajosepavamedina0112@gmail.com>
2023-08-20 20:56:42 -0500

    login

--- views/login.php
+++ views/login.php
@@ -6,71 +6,57 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
+        <title>Login</title>
         <link href="../css/styles.css" rel="stylesheet" />
-        <link href="../css/estilos.css" rel="stylesheet" />
+        <link href="../css/estilos-login.css" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicon-16x16.png">
         <link rel="manifest" href="../assets/img/site.webmanifest">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap" rel="stylesheet">
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
             <body class="login-page background-container">
-            <div id="layoutAuthentication">
-                <div id="layoutAuthentication_content">
-                    <main>
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-5">
-                                    <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                     <div class="card-header text-center">
-                                        <img src="../assets/img/logoSena.png" alt="Logo" class="logo-image">
-                                     </div>
-                                        <div class="card-body">
-                                            <?php
-
-                                            include '../comunes/alertas.php';
-                                            ?>
-                                            <form action="../php/validacion.php" method="post">
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="inputEmail" type="email" name="correo" placeholder="name@example.com" />
-                                                    <label for="inputEmail">Correo electrónico</label>
-                                                </div>
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="inputPassword" type="password" name="password" placeholder="Password" />
-                                                    <label for="inputPassword">Contraseña</label>
-                                                </div>
-                                                <div class="form-check mb-3">
-                                                    <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-                                                    <label class="form-check-label" for="inputRememberPassword">Recordar contraseña</label>
-                                                </div>
-                                                <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                    <a class="small" href="password.html">¿Olvidaste tu contraseña?</a>
-                                                    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
+            <main>
+    <div class="container">
+        <div class="row justify-content-between align-items-center" style="height: 100vh;">
+            <div class="col-lg-5">
+         
+                    <div class="card-body">
+                        <?php
+                        include '../comunes/alertas.php';
+                        ?>
+                        <form action="../php/validacion.php" method="post">
+                            <div class="form-floating mb-3 text-secondary">
+                                <input class="form-control" id="inputEmail" type="email" name="correo" placeholder="name@example.com" />
+                                <label for="inputEmail">Correo*</label>
                             </div>
-                        </div>
-                    </main>
-                </div>
-                <div id="layoutAuthentication_footer">
-                    <footer class="py-4 bg-light mt-auto">
-                        <div class="container-fluid px-4">
-                            <div class="d-flex align-items-center justify-content-between small">
-                                <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                                <div>
-                                    <a href="#">Privacy Policy</a>
-                                    &middot;
-                                    <a href="#">Terms &amp; Conditions</a>
-                                </div>
+                            <div class="form-floating mb-3  text-secondary">
+                                <input class="form-control" id="inputPassword" type="password" name="password" placeholder="Password" />
+                                <label for="inputPassword">Contraseña*</label>
                             </div>
-                        </div>
-                    </footer>
+                            <div class="d-flex align-items-center justify-content-center mt-4 mb-0 ">
+                                <button type="submit" class="btn btn-success w-100">Ingresar</button>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
+                                <a class="small" href="password.html">¿Olvidaste tu contraseña?</a>
+                            </div>
+                        </form>
+                    </div>
+                
+            </div>
+            <div class="col-lg-6 mb-5" id="color-titulo">
+                <h2 class="text-center mb-3 fw-lighter fs-1">Bienvenido(a)</h2>
+                <h2 class="text-center mb-3 fw-lighter fs-1">al portal</h2>
+                <h2 class="text-center fw-bold mb-3 fs-1 bold-font ">EDUCATIVO</h2>
+                <div class="card-header text-center mt-5">
+                <img src="../assets/img/sofia-plus.png" alt="Logo" class="logo-sofia " style="width: 250px;">   
                 </div>
             </div>
+        </div>
+    </div>
+</main>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>            
     </body>
